e7a8c92258a31992d85f3c12e7dba429b93530f8
kashkostwebsites <155662960+kashkostwebsites@users.noreply.github.com>
2024-04-24 07:16:28 +0100

    Create search.js

---
+++ search.js
@@ -0,0 +1,4 @@
+function search() {
+    var userInput = document.getElementById("userInput").value; // This assigns the user input to the variable userInput
+ window.location.href = userInput + '.html';
+}
